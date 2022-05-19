78f3cf09caabf8d3c2f6954f1cc65fa8424aa909
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-19 19:20:56 +0300

    create 1st tasck

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryItemsEL = makeGalleryItem(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsEL);
+galleryEl.addEventListener('click', onClickGalleryItem);
+
+function makeGalleryItem(array) {
+  return array
+    .map(el => {
+      return `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a> `;
+    })
+    .join('');
+}
+
+function onClickGalleryItem(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  lightbox.open(event);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  disableScroll: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
