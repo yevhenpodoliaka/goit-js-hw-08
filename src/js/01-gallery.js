37cf70058bb49811abde68a58a82f618bd9b20fe
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-20 13:13:57 +0300

    fixed gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,12 +3,11 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(galleryItems);
+
 
 const galleryEl = document.querySelector('.gallery');
 const galleryItemsEL = makeGalleryItem(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', galleryItemsEL);
-galleryEl.addEventListener('click', onClickGalleryItem);
 
 function makeGalleryItem(array) {
   return array
@@ -20,16 +19,7 @@ function makeGalleryItem(array) {
     .join('');
 }
 
-function onClickGalleryItem(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  lightbox.open(event);
-}
-
-const lightbox = new SimpleLightbox('.gallery a', {
+new SimpleLightbox('.gallery a', {
   disableScroll: true,
   captionsData: 'alt',
   captionDelay: 250,
